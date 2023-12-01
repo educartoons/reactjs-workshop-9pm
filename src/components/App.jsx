e18e6a536a40124b48a7db6d1a93331bcb6f37cf
Eduar Apaza <eapaza@learnsoft.com>
2023-11-30 22:43:58 -0500

    Initial settings

---
+++ src/components/App.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+export default function App() {
+  // JSX
+  return <h1>Welcome to ReactJS</h1>;
+}
---
+++ src/components/utils.js
@@ -0,0 +1,3 @@
+export function convertStringToNumber(str) {
+  return Number(str);
+}
