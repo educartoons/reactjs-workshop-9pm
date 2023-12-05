1d5ff9e110ae708a1c98c89edb2a4277c9634017
Eduar Apaza <eapaza@learnsoft.com>
2023-12-04 23:21:44 -0500

    Added Todo List

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,33 +1,119 @@
 import React, { useState, useEffect } from "react";
-import Header from "./Header";
-import Footer from "./Footer";
-import Body from "./Body";
 
-function login() {
-  return {
-    username: "educartoons",
-    email: "educartoons@gmail.com",
-  };
-}
+// {
+//   name: string,
+//   done: false
+// }
 
 export default function App() {
-  const [user, setUser] = useState();
-  const [cart, setCart] = useState([]);
+  const [todo, setTodo] = useState([]);
+  const [task, setTask] = useState("");
+  const [notify, setNotify] = useState(false);
+  const [disabled, setDisabled] = useState(true);
 
   useEffect(() => {
-    setUser(login());
+    const newTodo = localStorage.getItem("todo");
+    if (newTodo) {
+      setTodo(JSON.parse(newTodo));
+    }
   }, []);
 
-  const handleAddItemToCart = (item) => {
-    setCart([...cart, item]);
+  useEffect(() => {
+    console.log("Esto solo se ejecuta cuando el estado todo cambia");
+  }, [todo]);
+
+  const handleAddTask = () => {
+    if (task.trim() === "") {
+      setNotify(true);
+      setTask("");
+      return;
+    }
+
+    const newTodo = [
+      ...todo,
+      {
+        name: task,
+        done: false,
+      },
+    ];
+
+    setTodo(newTodo);
+    setTask("");
+
+    localStorage.setItem("todo", JSON.stringify(newTodo));
+  };
+
+  const handleChangeTask = (e) => {
+    setNotify(false);
+    setTask(e.target.value);
+    if (e.target.value.trim() === "") {
+      setDisabled(true);
+    } else {
+      setDisabled(false);
+    }
+  };
+
+  const handleMarkDoneTask = (index) => {
+    const updatedTodoList = todo.map((element, idx) => {
+      if (idx === index) {
+        return {
+          ...element,
+          done: true,
+        };
+      } else {
+        return element;
+      }
+    });
+
+    setTodo(updatedTodoList);
+    localStorage.setItem("todo", JSON.stringify(updatedTodoList));
+  };
+
+  const handleRemoveTask = (index) => {
+    const newTodo = todo.filter((element, idx) => idx !== index);
+    setTodo(newTodo);
   };
 
   // JSX
   return (
     <div>
-      <Header />
-      <Body user={user} handleAddItemToCart={handleAddItemToCart} />
-      <Footer />
+      <h2>TODO LIST</h2>
+      <ul>
+        {todo.map((element, index) => {
+          return element.done === false ? (
+            <li key={index}>
+              {element.name}{" "}
+              <button onClick={() => handleMarkDoneTask(index)}>✔️</button>{" "}
+              <button onClick={() => handleRemoveTask(index)}>❌</button>
+            </li>
+          ) : null;
+        })}
+      </ul>
+      <div>
+        <input value={task} type="text" onChange={(e) => handleChangeTask(e)} />
+        <button disabled={disabled} onClick={handleAddTask}>
+          Crear Tarea
+        </button>
+        {notify === true ? (
+          <p
+            style={{
+              color: "red",
+              border: "1px dotted red",
+              padding: "0.5em 1em",
+            }}
+          >
+            Necesitas agregar algo de texto para guardar una tarea.
+          </p>
+        ) : null}
+      </div>
+
+      <ul>
+        {todo.map((element, index) => {
+          return element.done === true ? (
+            <li key={index}>{element.name}</li>
+          ) : null;
+        })}
+      </ul>
     </div>
   );
 }
